3782062759305bc3dec67a85344838b3502d32dd
Bupé <ben@bupe.dev>
2024-12-01 00:29:05 +1000

    Added solution for Day 1 of 2021

---
+++ Utilities.cs
@@ -0,0 +1,19 @@
+namespace AOC;
+
+public static class Utilities {
+    public static IEnumerable<T[]> SlidingWindow<T>(this IEnumerable<T> source, int windowSize) {
+        using var enumerator = source.GetEnumerator();
+        var queue = new Queue<T>();
+
+        for (var i = 0; i < windowSize - 1; i++) {
+            if (!enumerator.MoveNext()) yield break;
+            queue.Enqueue(enumerator.Current);
+        }
+
+        while (enumerator.MoveNext()) {
+            queue.Enqueue(enumerator.Current);
+            yield return queue.ToArray();
+            queue.Dequeue();
+        }
+    }
+}
---
+++ Y2021/D01SonarSweep.cs
@@ -0,0 +1,21 @@
+namespace AOC.Y2021;
+
+public class D01SonarSweep() : Solution(2021, 1) {
+    protected override object GetPart1Result(string input) {
+        return input
+            .Split(Environment.NewLine)
+            .Select(int.Parse)
+            .SlidingWindow(2)
+            .Count(p => p[1] > p[0]);
+    }
+
+    protected override object GetPart2Result(string input) {
+        return input
+            .Split(Environment.NewLine)
+            .Select(int.Parse)
+            .SlidingWindow(3)
+            .Select(t => t.Sum())
+            .SlidingWindow(2)
+            .Count(p => p[1] > p[0]);
+    }
+}
