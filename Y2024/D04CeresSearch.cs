32fd705d7d4a21323f329146879e863ba3f0d201
Bupé <ben@bupe.dev>
2024-12-04 23:29:09 +1000

    Added solution for Day 4 of 2024

---
+++ Y2024/D04CeresSearch.cs
@@ -0,0 +1,33 @@
+using AOC.Utilities;
+using static AOC.Utilities.Miscellaneous;
+
+namespace AOC.Y2024;
+
+public class D04CeresSearch() : Solution(2024, 4) {
+    protected override object GetPart1Result(string input) {
+        return input
+            .ToMatrix()
+            .Transform(
+                (matrix, row, column) => GridDirections.All
+                    .Count(direction => new string(matrix.Slice(direction, row, column, 4)) == "XMAS")
+            )
+            .Flatten()
+            .Sum();
+    }
+
+    protected override object GetPart2Result(string input) {
+        return input
+            .ToMatrix()
+            .SlidingWindow(3, 3)
+            .Select(
+                matrix => Combine(matrix.GetIndices(), GridDirections.Diagonals)
+                    .Count(
+                        t => {
+                            var ((row, column), direction) = t;
+                            return new string(matrix.Slice(direction, row, column, 3)) == "MAS";
+                        }
+                    )
+            )
+            .Count(masCount => masCount == 2);
+    }
+}
