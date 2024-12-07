145c3888aabbb28596ebdf2ec160a0f5ff15a3ff
Bupé <ben@bupe.dev>
2024-12-07 14:20:47 +1000

    Fix dumb bug in CompassDirectionExtensions

--- Utilities/Grids/Directions.cs
+++ Utilities/Grids/Directions.cs
@@ -26,13 +26,13 @@ public static class CompassDirections {
 public static class CompassDirectionExtensions {
     public static GridIndex GetGridOffset(this CompassDirection compassDirection) {
         return compassDirection switch {
-            CompassDirection.North => new GridIndex(0, -1),
-            CompassDirection.NorthEast => new GridIndex(1, -1),
-            CompassDirection.East => new GridIndex(1, 0),
+            CompassDirection.North => new GridIndex(-1, 0),
+            CompassDirection.NorthEast => new GridIndex(-1, 1),
+            CompassDirection.East => new GridIndex(0, 1),
             CompassDirection.SouthEast => new GridIndex(1, 1),
-            CompassDirection.South => new GridIndex(0, 1),
-            CompassDirection.SouthWest => new GridIndex(-1, 1),
-            CompassDirection.West => new GridIndex(-1, 0),
+            CompassDirection.South => new GridIndex(1, 0),
+            CompassDirection.SouthWest => new GridIndex(1, -1),
+            CompassDirection.West => new GridIndex(0, -1),
             CompassDirection.NorthWest => new GridIndex(-1, -1),
             _ => throw new ArgumentOutOfRangeException(nameof(compassDirection), compassDirection, null)
         };
