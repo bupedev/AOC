129b8d86fd6e1ecdc2fb10414c8917e81fa38535
Ben Lewis <ben@bupe.dev>
2024-12-12 16:46:16 +1000

    WIP commit for solution of day 12 for 2024

--- Utilities/Grids/Grid.cs
+++ Utilities/Grids/Grid.cs
@@ -1,31 +1,39 @@
 namespace AOC.Utilities.Grids;
 
-public sealed record GridIndex(int Row, int Column) {
-    public static implicit operator GridIndex((int Row, int Column) values) {
+public sealed record GridIndex(int Row, int Column)
+{
+    public static implicit operator GridIndex((int Row, int Column) values)
+    {
         return new GridIndex(values.Row, values.Column);
     }
 
-    public static implicit operator (int Row, int Column)(GridIndex index) {
+    public static implicit operator (int Row, int Column)(GridIndex index)
+    {
         return (index.Row, index.Column);
     }
 
-    public static GridIndex operator +(GridIndex a, GridIndex b) {
+    public static GridIndex operator +(GridIndex a, GridIndex b)
+    {
         return (a.Row + b.Row, a.Column + b.Column);
     }
 
-    public static GridIndex operator -(GridIndex a, GridIndex b) {
+    public static GridIndex operator -(GridIndex a, GridIndex b)
+    {
         return (a.Row - b.Row, a.Column - b.Column);
     }
 
-    public static GridIndex operator *(int a, GridIndex b) {
+    public static GridIndex operator *(int a, GridIndex b)
+    {
         return new GridIndex(a * b.Row, a * b.Column);
     }
 }
 
-public sealed class Grid<T> {
+public sealed class Grid<T>
+{
     private readonly T[][] _data;
 
-    public Grid(T[][] data) {
+    public Grid(T[][] data)
+    {
         _data = data;
         RowCount = _data.Length;
         ColumnCount = _data[0].Length;
@@ -35,12 +43,14 @@ public sealed class Grid<T> {
 
     public int ColumnCount { get; }
 
-    public T this[GridIndex index] {
+    public T this[GridIndex index]
+    {
         get => this[index.Row, index.Column];
         set => this[index.Row, index.Column] = value;
     }
 
-    public T this[int row, int column] {
+    public T this[int row, int column]
+    {
         get => _data[row][column];
         set => _data[row][column] = value;
     }
@@ -52,23 +62,28 @@ public sealed class Grid<T> {
     public Grid<T> this[Range rowRange, Range columnRange] =>
         new(_data[rowRange].Select(row => row[columnRange]).ToArray());
 
-    public Grid<T> Clone() {
+    public Grid<T> Clone()
+    {
         return new Grid<T>(_data.Select(row => row.Select(columnValue => columnValue).ToArray()).ToArray());
     }
 
-    public IEnumerable<T[]> AsRows() {
+    public IEnumerable<T[]> AsRows()
+    {
         return _data;
     }
 
-    public IEnumerable<T[]> AsColumns() {
+    public IEnumerable<T[]> AsColumns()
+    {
         return (..ColumnCount).Iterate().Select(column => this[.., column]);
     }
 
-    public IEnumerable<GridIndex> GetIndices() {
+    public IEnumerable<GridIndex> GetIndices()
+    {
         return GetIndices(.., ..);
     }
 
-    public IEnumerable<GridIndex> GetIndices(Range rows, Range columns) {
+    public IEnumerable<GridIndex> GetIndices(Range rows, Range columns)
+    {
         return rows
             .ToSimpleRange(RowCount)
             .Iterate()
@@ -77,32 +92,39 @@ public sealed class Grid<T> {
             );
     }
 
-    public bool InRange(GridIndex index) {
+    public bool InRange(GridIndex index)
+    {
         return (..RowCount).Includes(index.Row) && (..ColumnCount).Includes(index.Column);
     }
 
-    public Grid<T> Transpose() {
+    public Grid<T> Transpose()
+    {
         return new Grid<T>(AsColumns().ToArray());
     }
 
-    public Grid<T> FilterRows(Func<T[], bool> rowFilter) {
+    public Grid<T> FilterRows(Func<T[], bool> rowFilter)
+    {
         return new Grid<T>(AsRows().Where(rowFilter).ToArray());
     }
 
-    public Grid<T> FilterColumns(Func<T[], bool> columnFilter) {
+    public Grid<T> FilterColumns(Func<T[], bool> columnFilter)
+    {
         return new Grid<T>(AsColumns().Where(columnFilter).ToArray()).Transpose();
     }
 
-    public Grid<T> TransformRows(Func<T[], IEnumerable<T>> rowTransformation) {
+    public Grid<T> TransformRows(Func<T[], IEnumerable<T>> rowTransformation)
+    {
         return new Grid<T>(AsRows().Select(rowTransformation).Select(row => row.ToArray()).ToArray());
     }
 
-    public Grid<T> TransformColumns(Func<T[], IEnumerable<T>> columnTransformation) {
+    public Grid<T> TransformColumns(Func<T[], IEnumerable<T>> columnTransformation)
+    {
         return new Grid<T>(AsColumns().Select(columnTransformation).Select(column => column.ToArray()).ToArray())
             .Transpose();
     }
 
-    public T[] Slice(CompassDirection compassDirection, GridIndex index, int length) {
+    public T[] Slice(CompassDirection compassDirection, GridIndex index, int length)
+    {
         return (..length)
             .Iterate()
             .Select(step => step * (index + compassDirection.GetGridOffset()))
@@ -111,7 +133,8 @@ public sealed class Grid<T> {
             .ToArray();
     }
 
-    public Grid<TOut> Transform<TOut>(Func<Grid<T>, GridIndex, TOut> transformation) {
+    public Grid<TOut> Transform<TOut>(Func<Grid<T>, GridIndex, TOut> transformation)
+    {
         return new Grid<TOut>(
             (..RowCount)
             .Iterate()
@@ -125,28 +148,59 @@ public sealed class Grid<T> {
         );
     }
 
-    public Grid<TOut> Transform<TOut>(Func<T, TOut> transformation) {
+    public Grid<TOut> Transform<TOut>(Func<T, TOut> transformation)
+    {
         return Transform<TOut>((grid, index) => transformation(grid[index]));
     }
 
-    public IEnumerable<GridIndex> FindIndices(Func<Grid<T>, GridIndex, bool> predicate) {
+    public IEnumerable<GridIndex> FindIndices(Func<Grid<T>, GridIndex, bool> predicate)
+    {
         return GetIndices().Where(index => predicate(this, index));
     }
 
-    public IEnumerable<GridIndex> FindIndices(Func<T, bool> predicate) {
+    public IEnumerable<GridIndex> FindIndices(Func<T, bool> predicate)
+    {
         return FindIndices((grid, index) => predicate(grid[index]));
     }
 
-    public IEnumerable<T> Flatten() {
+    public IEnumerable<T> Flatten()
+    {
         return GetIndices().Select(index => this[index]);
     }
 
-    public IEnumerable<Grid<T>> SlidingWindow(int rowCount, int columnCount) {
+    public IEnumerable<Grid<T>> SlidingWindow(int rowCount, int columnCount)
+    {
         return GetIndices(..^(rowCount - 1), ..^(columnCount - 1))
             .Select(index => this[index.Row..(index.Row + rowCount), index.Column..(index.Column + columnCount)]);
     }
 
-    public string Stringify(bool commaDelimitRows = true) {
+    public IEnumerable<GridIndex> CardinalSearch(GridIndex gridIndex,
+        Func<Grid<T>, GridIndex, GridIndex, bool> inclusionPredicate)
+    {
+        var inclusionSet = new HashSet<GridIndex>();
+        var indexQueue = new Queue<GridIndex>();
+
+        inclusionSet.Add(gridIndex);
+        indexQueue.Enqueue(gridIndex);
+
+        while (indexQueue.Count > 0)
+        {
+            var target = indexQueue.Dequeue();
+            var candidates = CompassDirections.Cardinals.Select(x => x.GetGridOffset() + target)
+                .Where(adj => inclusionPredicate(this, target, adj)).Where(index => !inclusionSet.Contains(index));
+            foreach (var candidate in candidates)
+            {
+                inclusionSet.Add(candidate);
+                indexQueue.Enqueue(candidate);
+            }
+        }
+        
+        return inclusionSet;
+        
+    }
+
+    public string Stringify(bool commaDelimitRows = true)
+    {
         var stringGrid = Transform((grid, index) => grid[index]?.ToString() ?? string.Empty);
         var columnWidth = stringGrid.AsColumns().SelectMany(column => column.Select(text => text.Length)).Max();
 
---
+++ Y2024/D12.cs
@@ -0,0 +1,82 @@
+﻿using AOC.Utilities;
+using AOC.Utilities.Grids;
+
+namespace AOC.Y2024;
+
+public class D12() : Solution(2024, 12)
+{
+    protected override object GetPart1Result(string input)
+    {
+        return input.ToGrid().Then(map =>
+        {
+            return GetRegions(map).Select(region =>
+            {
+                var area = region.Count();
+                var perimeter = region.Sum(tile =>
+                    CompassDirections.Cardinals.Select(dir => dir.GetGridOffset() + tile)
+                        .Count(adj => !map.InRange(adj) || map[adj] != map[tile]));
+
+                return area * perimeter;
+            }).Sum();
+        });
+    }
+
+    protected override object GetPart2Result(string input)
+    {
+        return input.ToGrid().Then(map => GetRegions(map).Select(region =>
+        {
+            var area = region.Count();
+            var perimeter = region.Where(tile =>
+                                    !map.InRange(CompassDirection.North.GetGridOffset() + tile) ||
+                                    (map[CompassDirection.North.GetGridOffset() + tile] != map[tile]))
+                                .GroupBy(tile => tile.Row)
+                                .Select(row => CountContiguousIntegers(
+                                    row
+                                        .Select(tile => tile.Column))).Sum() +
+                            region.Where(tile =>
+                                    !map.InRange(CompassDirection.South.GetGridOffset() + tile) ||
+                                    (map[CompassDirection.South.GetGridOffset() + tile] != map[tile]))
+                                .GroupBy(tile => tile.Row)
+                                .Select(row => CountContiguousIntegers(
+                                    row
+                                        .Select(tile => tile.Column))).Sum() +
+                            region.Where(tile =>
+                                    !map.InRange(CompassDirection.East.GetGridOffset() + tile) ||
+                                    (map[CompassDirection.East.GetGridOffset() + tile] != map[tile]))
+                                .GroupBy(tile => tile.Column)
+                                .Select(row => CountContiguousIntegers(
+                                    row
+                                        .Select(tile => tile.Row))).Sum() +
+                            region.Where(tile =>
+                                    !map.InRange(CompassDirection.West.GetGridOffset() + tile) ||
+                                    (map[CompassDirection.West.GetGridOffset() + tile] != map[tile]))
+                                .GroupBy(tile => tile.Column)
+                                .Select(row => CountContiguousIntegers(
+                                    row
+                                        .Select(tile => tile.Row))).Sum();
+
+
+
+            return area * perimeter;
+        }).Sum());
+    }
+    
+    private IEnumerable<HashSet<GridIndex>> GetRegions(Grid<char> map)
+    {
+        return map.GetIndices().Aggregate((Visited: new HashSet<GridIndex>(), Regions: new List<HashSet<GridIndex>>()),
+            (state, index) =>
+            {
+                if (state.Visited.Contains(index)) return state;
+                var region = map.CardinalSearch(index,
+                    (grid, prev, next) => grid.InRange(next) && grid[prev] == grid[next]).ToHashSet();
+                state.Visited.UnionWith(region);
+                state.Regions.Add(region);
+                return state;
+            }).Regions;
+    }
+
+    private static int CountContiguousIntegers(IEnumerable<int> values)
+    {
+        return values.OrderBy(x => x).Pairwise().Count(pair => pair.Second != pair.First + 1) + 1;
+    }
+}
