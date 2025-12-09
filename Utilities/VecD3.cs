a0127f001ef66734bbe4c730d7ce350d822921b6
Ben Lewis <ben@bupe.dev>
2025-12-09 21:44:51 +1000

    Add solution for Day 8 of 2025

---
+++ Utilities/VecD3.cs
@@ -0,0 +1,21 @@
+﻿namespace AOC.Utilities;
+
+public sealed record VecD3(double X, double Y, double Z)
+{
+    public static implicit operator VecD3((double, double, double) t) => new(t.Item1, t.Item2, t.Item3);
+    
+    public static implicit operator VecD3(double[] arr)
+    {
+        return arr.Length != 3 
+            ? throw new ArgumentException($"Array must contain exactly 3 elements, but got {arr.Length}", nameof(arr)) 
+            : new VecD3(arr[0], arr[1], arr[2]);
+    }
+    
+    public static double EuclideanDistance(VecD3 a, VecD3 b)
+    {
+        var dx = a.X - b.X;
+        var dy = a.Y - b.Y;
+        var dz = a.Z - b.Z;
+        return Math.Sqrt(dx * dx + dy * dy + dz * dz);
+    }
+}
---
+++ Y2025/D08.cs
@@ -0,0 +1,86 @@
+﻿using AOC.Utilities;
+
+namespace AOC.Y2025;
+
+public class D08() : Solution(2025, 8)
+{
+    protected override object GetPart1Result(string input)
+    {
+        var circuits = new List<HashSet<int>>();
+        var boxes = GetBoxes(input);
+        var boxPairs = GetBoxPairsByDistance(boxes);
+
+        foreach (var boxPair in boxPairs.Take(1000))
+        {
+            ConnectBoxes(boxPair, circuits);
+        }
+
+        return circuits.Select(c => c.Count).OrderDescending().Take(3).Product();
+    }
+
+    protected override object GetPart2Result(string input)
+    {
+        var circuits = new List<HashSet<int>>();
+        var boxes = GetBoxes(input);
+        var boxPairs = GetBoxPairsByDistance(boxes).AsEnumerable();
+
+        foreach (var boxPair in boxPairs)
+        {
+            ConnectBoxes(boxPair, circuits);
+            if (circuits.Count == 1 && circuits[0].Count == boxes.Length)
+            {
+                return boxPair.A.Position.X * boxPair.B.Position.X;
+            }
+        }
+
+        return double.MaxValue;
+    }
+
+    private sealed record Box(int Index, VecD3 Position);
+
+    private static Box[] GetBoxes(string input)
+    {
+        return input
+            .SplitOnNewLines()
+            .Select(line => line.Split(',').Select(double.Parse).ToArray())
+            .Index()
+            .Select(arr => new Box(arr.Index, arr.Item))
+            .ToArray();
+    }
+
+    private static (Box A, Box B)[] GetBoxPairsByDistance(Box[] boxes)
+    {
+        return boxes
+            .GetUniquePairs()
+            .OrderBy(p => VecD3.EuclideanDistance(p.First.Position, p.Second.Position))
+            .ToArray();
+    }
+
+    private static void ConnectBoxes((Box A, Box B) boxPair, List<HashSet<int>> circuits)
+    {
+        var aSet = circuits.SingleOrDefault(c => c.Contains(boxPair.A.Index));
+        var bSet = circuits.SingleOrDefault(c => c.Contains(boxPair.B.Index));
+
+        if (aSet is not null && bSet is not null)
+        {
+            if (aSet != bSet)
+            {
+                circuits.Remove(aSet);
+                circuits.Remove(bSet);
+                circuits.Add(aSet.Union(bSet).ToHashSet());
+            }
+        }
+        else if (aSet is not null)
+        {
+            aSet.Add(boxPair.B.Index);
+        }
+        else if (bSet is not null)
+        {
+            bSet.Add(boxPair.A.Index);
+        }
+        else
+        {
+            circuits.Add([boxPair.A.Index, boxPair.B.Index]);
+        }
+    }
+}
